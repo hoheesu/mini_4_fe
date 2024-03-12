6b1b8bb1049c174818db4fd8828f0a1cd41cdcb9
윤준수 <99115509+hoheesu@users.noreply.github.com>
2024-03-13 01:13:40 +0900

    CHORE: 프로젝트 시작전 초기세팅

---
+++ src/router/router.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Main from "../pages/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+    ],
+  },
+]);
